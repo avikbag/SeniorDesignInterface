861376bdc8d2d8c47b27383f36c6fc5e382970bb
Avik Bag <avikbag@gmail.com>
2017-02-14 23:38:34 -0500

    Added CORS settings to the origin

--- web.js
+++ web.js
@@ -45,7 +45,25 @@ app.get('/sign-s3', (req, res) => {
           res.end();
         });
 });
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 /*
  * Respond to POST requests to /submit_form.
  * This function needs to be completed to handle the information in
